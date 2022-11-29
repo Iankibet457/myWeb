60462716d94655cdf0ae8274d0876b7071dc833e
iankibetmosonik <ian.kibet@student.moringaschool.com>
2022-11-29 18:01:37 +0300

    Update html attributes

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 // const h2 = document.createElement("h2");
 // h2.textContent = "This content added by JavaScript";
 // document.querySelector("body").appendChild(h2);
+
+const btn =document.getElementsByTagName("button");
+btn.addEventListener("click",function(){
+    alert("Downloading...");
+});
